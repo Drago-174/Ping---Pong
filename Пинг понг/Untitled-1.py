1e9ad6affc2cc8f4d9b876700ec7d9abe2198983
Drago-174 <103132162+Drago-174@users.noreply.github.com>
2022-04-13 18:57:51 +0300

    Update Untitled-1.py

--- Пинг понг/Untitled-1.py
+++ Пинг понг/Untitled-1.py
@@ -7,6 +7,33 @@ window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x,size_z))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update_r(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y >5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+       
+    def update_l(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_W] and self.rect.y >5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_S] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+       
 game = True
 
 finish = False
