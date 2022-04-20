39319a4fe9c9e352a9e6c17d651b2da0da9ab5e1
Drago-174 <103132162+Drago-174@users.noreply.github.com>
2022-04-20 18:23:24 +0300

    Update Untitled-1.py

--- Пинг понг/Untitled-1.py
+++ Пинг понг/Untitled-1.py
@@ -8,7 +8,7 @@ window.fill(back)
 
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, size_x,size_z,player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x,size_z))
         self.speed = player_speed
@@ -29,9 +29,9 @@ class Player(GameSprite):
        
     def update_l(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_W] and self.rect.y >5:
+        if keys_pressed[K_w] and self.rect.y >5:
             self.rect.y -= self.speed
-        if keys_pressed[K_S] and self.rect.y < win_width - 80:
+        if keys_pressed[K_s] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
        
 game = True
@@ -43,11 +43,29 @@ clock = time.Clock()
 FPS = 60
 
 
+racket = Player('12.png' , 30,200,200, 200, 15)
+racket2 = Player('12.png' , 600,200,200, 200,15)
+
 while game:
 
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+
+
+
+    if finish != True:
+
+        window.fill(back)
+
+        racket.update_l()
+        racket2.update_r()
+
+
+        racket.reset()
+        racket2.reset()
+
 
     display.update()
     clock.tick(FPS)
