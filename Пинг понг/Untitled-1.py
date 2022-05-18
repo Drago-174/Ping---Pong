0f1cf4eab16aa9127968a4519b7cccf5c9549223
Drago-174 <103132162+Drago-174@users.noreply.github.com>
2022-05-18 18:26:47 +0300

    Update Untitled-1.py

--- Пинг понг/Untitled-1.py
+++ Пинг понг/Untitled-1.py
@@ -1,17 +1,20 @@
-from pygame import *
+import pygame
+import time
 
+time.sleep(3)
 
 back = (42, 169, 219)
 win_width = 800
 win_height = 700
-window = display.set_mode((win_width, win_height))
+window = pygame.display.set_mode((win_width, win_height))
+
 window.fill(back)
 
 
-class GameSprite(sprite.Sprite):
+class GameSprite(pygame.sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x,size_z,player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (size_x,size_z))
+        self.image = pygame.transform.scale(pygame.image.load(player_image), (size_x,size_z))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -22,35 +25,34 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def update_r(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_UP] and self.rect.y >5:
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_UP] and self.rect.y >5:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < win_width - 80:
+        if keys_pressed[pygame.K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
        
     def update_l(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_w] and self.rect.y >5:
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_w] and self.rect.y >5:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < win_width - 80:
+        if keys_pressed[pygame.K_s] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
        
 game = True
 
 finish = False
 
-clock = time.Clock()
+clock = pygame.time.Clock()
 
 FPS = 60
 
 line = 0
 
 
-mixer.init()
-mixer.music.load('213.ogg')
-mixer.music.set_volume(2.2)
-mixer.music.play()
-ssound = mixer.Sound('02934.ogg')
+pygame.mixer.init()
+pygame.mixer.music.load('213.ogg')
+pygame.mixer.music.set_volume(1.2)
+pygame.mixer.music.play()
 
 racket = Player('22222222.jpg' , -150,200,200, 145, 15)
 racket2 = Player('1111111.jpg' , 750,200,200, 145, 15)
@@ -60,10 +62,11 @@ ball = GameSprite('22.png', 280, 200,40, 40, 15)
 dx = 3
 dy = 3
 
-font.init()
-font = font.Font(None, 35)
-lose1 = font.render('PlAYER 1 lOSE', True ,(180, 0, 0))
-lose2 = font.render('PlAYER 2 lOSE', True ,(180, 0, 0))
+pygame.font.init()
+#font = font.Font(None, 36)
+#pygame.lose1 = pygame.font.render('PlAYER 1 lOSE', True ,(180, 0, 0))
+#pygame.lose2 = pygame.font.render('PlAYER 2 lOSE', True ,(180, 0, 0))
+
 
 
 score_left = 0
@@ -76,8 +79,8 @@ score_right = 0
 Balls = -2
 while game:
 
-    for e in event.get():
-        if e.type == QUIT:
+    for e in pygame.event.get():
+        if type == pygame.QUIT:
             game = False
 
 
@@ -90,7 +93,7 @@ while game:
         ball.rect.x += dx
         ball.rect.y += dy
 
-        if sprite.collide_rect(racket, ball) or sprite.collide_rect(racket2, ball):
+        if pygame.sprite.collide_rect(racket, ball) or pygame.sprite.collide_rect(racket2, ball):
             dx *= -1
 
         if ball.rect.y < 0 or ball.rect.y > win_height - 40:
@@ -110,10 +113,10 @@ while game:
 
         
         
-        score_l = font.render(str(score_left), True , (0, 0, 0))
-        score_r = font.render(str(score_right), True , (0, 0, 0))
-        window.blit(score_l, (10,10))
-        window.blit(score_r, (win_width-25, 10))
+        #score_l = pygame.font.render(str(score_left), True , (0, 0, 0))
+        #score_r = pygame.font.render(str(score_right), True , (0, 0, 0))
+        #window.blit(score_l, (10,10))
+        #window.blit(score_r, (win_width-25, 10))
 
 
         if ball.rect.x > win_width:
@@ -141,5 +144,5 @@ while game:
         ball.reset()
 
 
-    display.update()
+    pygame.display.update()
     clock.tick(FPS)
